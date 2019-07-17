f49cdebee08242a3bba18219bc9bc1481dba7d71
ALBLWI <alblwi@gmail.com>
2019-07-17 07:46:31 +0300

    Changing
    This one has some changing to be mo accurate

---
+++ MemeMeV1 2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MemeMeV1
+//
+//  Created by AHMED ALBLWI on 13/04/2019.
+//  Copyright © 2019 AHMED ALBLWI. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    
+
+
+}
+
