faca45b10221cefbfd0f58d14fedf5d33219cce1
ALBLWI <alblwi@gmail.com>
2019-07-18 23:49:10 +0300

    add class Keyboard for students

---
+++ MemeMeV1/KeyBoardFunctions.swift
@@ -0,0 +1,12 @@
+//
+//  KeyBoardFunctions.swift
+
+//
+//  Created by AHMED ALBLWI on 18/07/2019.
+//  Copyright © 2019 AHMED ALBLWI. All rights reserved.
+//
+
+import Foundation
+
+// TO DO
+// Try to add all keyboard finctions here to make ur main class clean :)
